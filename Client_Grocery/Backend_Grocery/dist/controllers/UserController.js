0b7d7e8b9ae87e8b3ebd847869f4f1610e6e650f
soumiaessabiry <soumiaessabiry.se@gmail.com>
2023-02-12 21:22:06 +0100

    ajouter client-floder

--- Client_Grocery/Backend_Grocery/dist/controllers/UserController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Login = void 0;
-// const env=require("dotenv");
-// const userModel=require("../Model/UserModel");
-const Login = (req, res, next) => {
-    res.json({ message: "hello login" });
-};
-exports.Login = Login;
--- Client_Grocery/Backend_Grocery/dist/routes/AuthRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const UserController_1 = require("../controllers/UserController");
-const router = (0, express_1.Router)();
-router.get("/", UserController_1.Login);
-exports.default = router;
